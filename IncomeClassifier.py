98aca9ae0ea5d5dba97b178b4d2f35557a836489
Amit Yadav <ayadav3@usfca.edu>
2014-12-28 22:50:55 -0800

    update

--- IncomeClassifier.py
+++ IncomeClassifier.py
@@ -121,3 +121,147 @@ def sampling(pos_index, neg_index, size):
     return indices
 
 
+def dataNormalization(X, num):
+    """
+
+    :param X:
+    :param num:
+    :return:
+    """
+    if num == 1:
+        normX = scale(X)
+    elif num == 2:
+        min_max_scaler = MinMaxScaler()
+        normX = min_max_scaler.fit_transform(X)
+    else:
+        print 'wrong parameter for data normalization'
+
+    return normX
+
+
+def decisionTree(X, y, X_test, y_test):
+    """
+
+    :param X:
+    :param y:
+    :param X_test:
+    :param y_test:
+    :return:
+    """
+    clf = tree.DecisionTreeClassifier(criterion='gini', max_depth=2)
+    clf.fit(X, y)
+    ypred = clf.predict(X_test[:])
+    print f1_score(y_test, ypred, average=None)
+
+
+def randomForest(X, y, X_test, y_test):
+    """
+
+    :param X:
+    :param y:
+    :param X_test:
+    :param y_test:
+    :return:
+    """
+    clf = RandomForestClassifier(n_estimators=500, criterion='entropy', random_state=33, n_jobs=-1, max_features=50)
+    clf.fit(X, y)
+    ypred = clf.predict(X_test[:])
+    print f1_score(y_test, ypred, average=None)
+    # print cross_val_score(clf, X, y, cv = 3, scoring = 'accuracy').mean()
+
+
+def logistic(X, y, X_test, y_test):
+    """
+
+    :param X:
+    :param y:
+    :param X_test:
+    :param y_test:
+    :return:
+    """
+    clf = LogisticRegression(penalty='l2', C=0.5, random_state=33)
+    clf.fit(X, y)
+    ypred = clf.predict(X_test[:])
+    # print clf.score(X, y)
+    print f1_score(y_test, ypred, average=None)
+    #print cross_val_score(clf, X, y, cv = 3, scoring = 'accuracy').mean()
+
+
+def KNN(X, y, X_test, y_test):
+    """
+
+    :param X:
+    :param y:
+    :param X_test:
+    :param y_test:
+    :return:
+    """
+    clf = KNeighborsClassifier(n_neighbors=5)
+    print cross_val_score(clf, X, y, cv=5, scoring='accuracy').mean()
+
+
+def SVM(X, y, X_test, y_test):
+    """
+
+    :param X:
+    :param y:
+    :param X_test:
+    :param y_test:
+    :return:
+    """
+    clf = SVC(C=10)
+    clf.fit(X, y)
+    ypred = clf.predict(X_test[:])
+    # print clf.score(X, y)
+    print f1_score(y_test, ypred, average=None)
+    #print clf.score(X_test, y_test)
+    #print f1_score(y_test, ypred, average = None)
+    #print clf.score(X_test, y_test)
+    #print cross_val_score(clf, X, y, cv = 5, scoring = 'accuracy').mean()
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('census-income.data', header=None, delimiter=',')  # read the original data
+    data_test = pd.read_csv('census-income.test', header=None, delimiter=',')
+
+    data.columns = ['AAGE', 'ACLSWKR', 'ADTIND', 'ADTOCC', 'AHGA', 'AHRSPAY',
+                    'AHSCOL', 'AMARITL', 'AMJIND', 'AMJOCC', 'ARACE', 'AREORGN',
+                    'ASEX', 'AUNMEM', 'AUNTYPE', 'AWKSTAT', 'CAPGAIN', 'CAPLOSS',
+                    'DIVVAL', 'FILESTAT', 'GRINREG', 'GRINST', 'HHDFMX', 'HHDREL',
+                    'MARSUPWT', 'MIGMTR1', 'MIGMTR3', 'MIGMTR4', 'MIGSAME', 'MIGSUN',
+                    'NOEMP', 'PARENT', 'PEFNTVTY', 'PEMNTVTY', 'PENATVTY', 'PRCITSHP',
+                    'SEOTR', 'VETQVA', 'VETYN', 'WKSWORK', 'YEAR',
+                    'target']  # original columns name from the supplementary file of data set
+
+    data_test.columns = data.columns
+    # print data_test.shape
+
+    continuous_columns = ['AAGE', 'AHRSPAY', 'DIVVAL', 'NOEMP', 'CAPGAIN', 'CAPLOSS', 'WKSWORK', 'MARSUPWT',
+                          'target']  # names of columns with continous value( I put the 'target' also here)
+    binary_columns = ['ASEX']  # columns with binary value
+
+    data = drop_columns(data)  # drop some colums
+    data = drop_rows(data)  # drop some rows, since some rows have missing value
+    data_test = drop_columns(data_test)
+    data_test = drop_rows(data_test)
+
+    dummy_columns = [col for col in data.columns.values if col not in continuous_columns if
+                     col not in binary_columns]  # names of columns with dummy features
+
+    data = data_transformation(data, continuous_columns, dummy_columns, binary_columns)  # transform the dataset
+    data_test = data_transformation(data_test, continuous_columns, dummy_columns, binary_columns)
+    X, y = original_data(data)  # split features and target
+    X_test, y_test = original_data(data_test)
+
+    pos_index, neg_index = np.where(y == 1)[0], np.where(y == 0)[
+        0]  # separate the indices of target between values of 1 and 0
+    indices = sampling(pos_index, neg_index, 50000)  # correct the imbalance of the training dataset
+    X, y = X[indices], y[
+        indices]  # get the final dataset for model fitting, this is for training set only, shuffling the data
+
+    # logistic(dataNormalization(X, 2), y, dataNormalization(X_test, 2), y_test)
+    #chooseF(X, y, X_test, y_test)
+
+    #randomForest(X, y, X_test, X, y, X_test, y_test)
+    #decisionTree(X, y, X_test, y_test)
+    #SVM(dataNormalization(X, 2), y) #dataNormalization(X_test, 2), y_test)
